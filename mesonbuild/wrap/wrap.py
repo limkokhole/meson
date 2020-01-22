limkokhole/meson

commit d08091756191981f1bd3c7741b412b95f965fe0a

wrap.py: catch connection error with WrapException
fixes #6130

wrap: more error verbosity

mesonbuild/wrap/wrap.py
@@ -68,17 +68,20 @@ def open_wrapdburl(urlstring: str) -> 'http.client.HTTPResponse':
             return urllib.request.urlopen(urlstring, timeout=req_timeout)# , context=build_ssl_context())
         except urllib.error.URLError:
             if not ssl_warning_printed:
-                print('SSL connection failed. Falling back to unencrypted connections.')
+                print('SSL connection failed. Falling back to unencrypted connections.', file=sys.stderr)
                 ssl_warning_printed = True
     if not ssl_warning_printed:
-        print('Warning: SSL not available, traffic not authenticated.',
-              file=sys.stderr)
+        print('Warning: SSL not available, traffic not authenticated.', file=sys.stderr)
         ssl_warning_printed = True
     # Trying to open SSL connection to wrapdb fails because the
     # certificate is not known.
     if urlstring.startswith('https'):
         urlstring = 'http' + urlstring[5:]
-    return urllib.request.urlopen(urlstring, timeout=req_timeout)
+    try:
+        return urllib.request.urlopen(urlstring, timeout=req_timeout)
+    except urllib.error.URLError:
+        raise WrapException('failed to get {} is the internet available?'.format(urlstring))
+
 
 class WrapException(MesonException):
     pass
@@ -306,7 +309,10 @@ class Resolver:
         if url.startswith('https://wrapdb.mesonbuild.com'):
             resp = open_wrapdburl(url)
         else:
-            resp = urllib.request.urlopen(url, timeout=req_timeout)
+            try:
+                resp = urllib.request.urlopen(url, timeout=req_timeout)
+            except urllib.error.URLError:
+                raise WrapException('could not get {} is the internet available?'.format(url))
         with contextlib.closing(resp) as resp:
             try:
                 dlsize = int(resp.info()['Content-Length'])

mesonbuild/wrap/wraptool.py
@@ -124,11 +124,11 @@ def update(options):
         raise SystemExit('Subprojects dir not found. Run this command in your source root directory.')
     wrapfile = os.path.join('subprojects', name + '.wrap')
     if not os.path.exists(wrapfile):
-        raise SystemExit('Project', name, 'is not in use.')
+        raise SystemExit('Project ' + name + ' is not in use.')
     (branch, revision, subdir, src_file, patch_file) = get_current_version(wrapfile)
     (new_branch, new_revision) = get_latest_version(name)
     if new_branch == branch and new_revision == revision:
-        print('Project', name, 'is already up to date.')
+        print('Project ' + name + ' is already up to date.')
         raise SystemExit
     update_wrap_file(wrapfile, name, new_branch, new_revision)
     shutil.rmtree(os.path.join('subprojects', subdir), ignore_errors=True)

test cases/common/91 dep fallback/tester.c
@@ -3,7 +3,7 @@
 #include<string.h>
 #include<stdio.h>
 
-int main(int argc, char **argv) {
+int main(void) {
     if(strcmp("bob", get_bob()) == 0) {
         printf("Bob is indeed bob.\n");
     } else {

commit 790ae620e773d5bff0c275de8f8aa62f247d13ca

wrap: Redirect stdin to DEVNULL when calling git
Without this git messes up the console and ANSI colors stop working on
Windows inside cmd.exe.

This broke in https://github.com/mesonbuild/meson/pull/6139. Also add
the same to all git calls. Missed this when I opened
https://github.com/mesonbuild/meson/pull/6255

mesonbuild/wrap/wrap.py
@@ -53,6 +53,9 @@ def quiet_git(cmd: T.List[str], workingdir: str) -> T.Tuple[bool, str]:
     if not GIT:
         return False, 'Git program not found.'
     pc = subprocess.run([GIT, '-C', workingdir] + cmd, universal_newlines=True,
+                        # Redirect stdin to DEVNULL otherwise git messes up the
+                        # console and ANSI colors stop working on Windows.
+                        stdin=subprocess.DEVNULL,
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if pc.returncode != 0:
         return False, pc.stderr
@@ -61,10 +64,11 @@ def quiet_git(cmd: T.List[str], workingdir: str) -> T.Tuple[bool, str]:
 def verbose_git(cmd: T.List[str], workingdir: str, check: bool = False) -> bool:
     if not GIT:
         return False
-    if check:
-        subprocess.check_call([GIT, '-C', workingdir] + cmd)
-        return True
-    return subprocess.run([GIT, '-C', workingdir] + cmd).returncode == 0
+    pc = subprocess.run([GIT, '-C', workingdir] + cmd,
+                        # Redirect stdin to DEVNULL otherwise git messes up the
+                        # console and ANSI colors stop working on Windows.
+                        stdin=subprocess.DEVNULL, check=check)
+    return pc.returncode == 0
 
 def whitelist_wrapdb(urlstr: str) -> urllib.parse.ParseResult:
     """ raises WrapException if not whitelisted subdomain """